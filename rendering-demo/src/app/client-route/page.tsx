e2e6cf7e7b3cbe222d8cea037bfded33654f30f9
Kamel-Amen <kamelamen38@gmail.com>
2024-12-06 22:37:13 +0200

    section 59 'client only'

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -3,33 +3,12 @@
 // import Slider from 'react-slick';
 // import 'slick-carousel/slick/slick.css';
 // import 'slick-carousel/slick/slick-theme.css';
-import { useTheme } from '@/components/theme-provider';
+// import { useTheme } from '@/components/theme-provider';
+import { clientSideFunction } from '@/utils/client-utils';
 
 export default function ClientRoutePage() {
-  const theme = useTheme();
+  // const theme = useTheme();
+  const result = clientSideFunction();
 
-  // const settings = {
-  //   dots: true,
-  // };
-
-  // return (
-  //   <div className='image-slider-container'>
-  //     <Slider {...settings}>
-  //       <div>
-  //         <img src='http://picsum.photos/400/200' />
-  //       </div>
-  //       <div>
-  //         <img src='http://picsum.photos/400/200' />
-  //       </div>
-  //       <div>
-  //         <img src='http://picsum.photos/400/200' />
-  //       </div>
-  //       <div>
-  //         <img src='http://picsum.photos/400/200' />
-  //       </div>
-  //     </Slider>
-  //   </div>
-  // );
-
-  return <h1 style={{ color: theme.colors.primary }}>Client Route</h1>;
+  return <h1>Client Route {result}</h1>;
 }
